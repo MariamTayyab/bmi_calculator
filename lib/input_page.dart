f49d5582e3400abee0ccc95b365580879d7f27fc
MariamTayyab <mariamtayyab08@gmail.com>
2020-11-26 01:49:11 +0500

    lec 60(deactive colors male and female)

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,10 +1,11 @@
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'icontextfile.dart';
 import 'containerfile.dart';
+const activeColor = Color(0xFF1D1E33);
+const deactiveColor = Color(0xFF111328);
 
 
-import 'package:flutter/material.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -12,6 +13,9 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleColor = deactiveColor;
+  Color femaleColor = deactiveColor;
+  void updateColor(int gender){}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +27,7 @@ class _InputPageState extends State<InputPage> {
           Expanded(child:Row(
             children: [
               Expanded(child: RefactorContainerCode(
-                Colors: Colors.orangeAccent,
+                Colors: maleColor,
                 CardWidget: new RepeatTextAndIcon(
                   iconData: FontAwesomeIcons.male,
                   label: 'MALE',
@@ -33,7 +37,7 @@ class _InputPageState extends State<InputPage> {
               ),
                 ),
               Expanded(child:  RefactorContainerCode(
-                Colors: Colors.red,
+                Colors: femaleColor,
                 CardWidget: new RepeatTextAndIcon(
                   iconData: FontAwesomeIcons.female,
                   label: 'FEMALE',
