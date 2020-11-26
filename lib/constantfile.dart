b771e7a4c98b4da7418ad8ee4602520bc521541a
MariamTayyab <mariamtayyab08@gmail.com>
2020-11-26 10:01:59 +0500

    lec 64(text style)

--- lib/constantfile.dart
+++ lib/constantfile.dart
@@ -4,6 +4,6 @@ const activeColor = Color(0xFF1D1E33);
 const deactiveColor = Color(0xFF111328);
 
 const klabelstyle=TextStyle(
-  fontSize: 30.0,
+  fontSize: 40.0,
   color: Color(0xFF8D8E98),
 );
