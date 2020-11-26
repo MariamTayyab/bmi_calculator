87b344032f1ee39cfe611ce89ffe0e816170def0
MariamTayyab <mariamtayyab08@gmail.com>
2020-11-26 16:00:29 +0500

    borderrad

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -235,7 +235,7 @@ class RoundIconButton extends StatelessWidget {
       onPressed: onPressed,
       child: Icon(icon),
       elevation: 6.0,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       fillColor: Colors.lightGreen,
       constraints: BoxConstraints.tightFor(width: 45.0, height: 45.0),
     );
